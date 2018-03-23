e77a5b14b1836c92da5c18dcd118efdf27840d4f
Mayurkumar Patel <mayurt20@gmail.com>
2018-03-23 10:50:26 +0530

    Completes ex7 for printing

---
+++ ex7.py
@@ -0,0 +1,17 @@
+print("this is printing exercise")
+print("let's do it")
+print("."*10)
+
+end1 = "M"
+end2 = "A"
+end3 = "Y"
+end4 = "U"
+end5 = "R"
+end6 = "P"
+end7 = "A"
+end8 = "T"
+end9 = "E"
+end10 = "L"
+
+print(f"my name is: {end1 + end2 + end3 + end4 + end5}", end = " ")
+print(end6 + end7 + end8 + end9 + end10)
