63b3966934405fa4247ad9e214c3a71d3384bf55
Mayurkumar Patel <mqyurt20@gmail.com>
2018-03-24 15:13:31 +0530

    Completes ex10 and ex11

---
+++ ex10.py
@@ -0,0 +1,13 @@
+my_cat = "\tTom is my cat"
+split_text = "I am splited at\nnew line"
+fat_cat  = """
+I 'll do listing
+\t* Cat
+\t* Dog
+\t* Buffelo
+"""
+height = "I\'m 5\'9\" tall"
+print(my_cat)
+print(split_text)
+print(fat_cat)
+print(height)
---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("Hey!! How old you are?", end=" ")
+age = input()
+print("May I know your weight?", end=" ")
+weight = input()
+print("One last question, what's your height?", end=" ")
+height = input()
+
+print(f"Well! you are {age} years old, your weight is {weight} and your height is {height}")
