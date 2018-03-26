c4255770fbfb8d2bca9a47c3650310d02cf4fec6
Mayurkumar Patel <mayurt20@gmail.com>
2018-03-26 10:51:49 +0530

    Completes ex8 and ex9

---
+++ ex9.py
@@ -0,0 +1,13 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay"
+print("these are the days: ", days)
+print("these are the months: ", months)
+print(
+"""
+Now I will write multiline texts
+This is very first line
+this is second line
+I can write more but I won't :-D
+But this is super easy
+"""
+)
