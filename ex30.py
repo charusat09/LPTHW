badc3e1dbfb6bcf96f0f1289bb391ccea99426c1
Mayurkumar Patel <mayurt20@gmail.com>
2018-03-27 12:01:25 +0530

    Completes ex29 and 30

---
+++ ex30.py
@@ -0,0 +1,11 @@
+people = 30
+cats = 30
+dogs = 15
+
+
+if people < cats:
+    print("Too many cats! The world is doomed!")
+elif people > cats:
+    print("Too many people")
+else:
+    print("No results")
